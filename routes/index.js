ae8e10a37cb47d3e533f6de77c389b0d130a5cf2
lisazaher <zaherlisa@gmail.com>
2018-09-08 20:28:29 -0400

    added necessary files to upload to heroku

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,23 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+/* GET home page. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+	var sectionContent = [
+	{
+		title: "Still",
+		body: "An app to help humans to manage anxiety"
 
+	},
+	{
+		Participant1: "Lisa Zaher",
+		Participant2: "Karen Zhao",
+		Participant3: "Jothi Ramaswamy",
+		Participant4: "Anushka Saxena",		
+
+	},
+	
+	]
+	res.render('index', { section: sectionContent });
+});
 module.exports = router;
--- start.js
+++ start.js
@@ -7,6 +7,7 @@ const PORT = process.env.PORT || 5000
 //from app
 var indexRouter = require('./routes/index');
 
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
@@ -15,6 +16,7 @@ app.use('/', indexRouter);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.get('/', (req, res) => res.render('index'));
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
