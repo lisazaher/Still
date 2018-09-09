43e0f5133c7c397661d5d1f36891f08426fa6760
lisazaher <zaherlisa@gmail.com>
2018-09-08 20:23:42 -0400

    added necessary files to upload to heroku

--- javascript.js
+++
@@ -1,3 +0,0 @@
-$(document).ready(function() {
-    $('.landing').fadeIn(1000);
-});
--- start.js
+++ start.js
@@ -13,7 +13,7 @@ app.use('/', indexRouter);
 
 
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'html');
+app.set('view engine', 'ejs');
 app.get('/', (req, res) => res.render('index'));
 
 // catch 404 and forward to error handler
