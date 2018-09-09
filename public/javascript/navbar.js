828d957b9480050e4a17ca66e231a9f5714e5cb1
lisazaher <zaherlisa@gmail.com>
2018-09-09 02:18:00 -0400

    added video

--- public/javascript/navbar.js
+++ public/javascript/navbar.js
@@ -1,18 +1,8 @@
-// When the user scrolls the page, execute myFunction 
-window.onscroll = function() {myFunction()};
 
 // Get the navbar
 var navbar = document.getElementById("navbar");
 
 
-// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
-function myFunction() {
-  if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky")
-  } else {
-    navbar.classList.remove("sticky");
-  }
-}
 
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
